6c0ffaa22d99a024520badcd60fee294aaaa9627
TimeoutHD <taubmann870@gmail.com>
2018-09-07 02:59:23 +0200

    Clean Up Code

--- gpath.py
+++ gpath.py
@@ -4,10 +4,10 @@ from datetime import datetime
 gmaps = googlemaps.Client(key='AIzaSyBzICJ4WWsoB3kyu8uoj2ht30Z-BY0kf_g')
 
 # Geocoding an address
-geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+#geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
 
 # Look up an address with reverse geocoding
-reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
+#reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
 
 # Request directions via public transit
 now = datetime.now()
