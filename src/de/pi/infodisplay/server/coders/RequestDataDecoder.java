209a1eb4d9045a37f820f266f886350b05d60660
robin.schupp <robin.schupp@r6011-09.schule.local>
2018-12-12 08:59:38 +0100

    Decoder

--- src/de/pi/infodisplay/server/coders/RequestDataDecoder.java
+++ src/de/pi/infodisplay/server/coders/RequestDataDecoder.java
@@ -1,19 +1,29 @@
 package de.pi.infodisplay.server.coders;
 
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.List;
 
+import de.pi.infodisplay.shared.packets.InfoDisplayPacket;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.ReplayingDecoder;
 
 public class RequestDataDecoder extends ReplayingDecoder<RequestData> {
 	
-	private final Charset charset = Charset.forName("UTF-8");
-
 	@Override
 	protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
 		RequestData data = new RequestData();
+		final List<InfoDisplayPacket> contents = new ArrayList<>();
+		for(int i = 0; i < in.readInt(); i++) {
+			int strLen = in.readInt();
+			String str = "";
+			for(int y = 0; y < strLen; y++) {
+				str += in.readChar();
+			}
+	        contents.add(InfoDisplayPacket.fromString(str));
+		}
+		data.setContents((InfoDisplayPacket[]) contents.toArray());
 	}
 
 }
