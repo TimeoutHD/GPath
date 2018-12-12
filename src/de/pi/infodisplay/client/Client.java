fe0f55437b56dee373c8b25cda3493f5918d2163
timo.taubmann <timo.taubmann@r6011-26.schule.local>
2018-12-12 08:54:05 +0100

    Add NettyClient and Change Main

--- src/de/pi/infodisplay/client/Client.java
+++ src/de/pi/infodisplay/client/Client.java
@@ -1,8 +1,16 @@
 package de.pi.infodisplay.client;
 
-public class Client {
+import de.pi.infodisplay.client.netty.NettyClient;
 
+public class Client {
+	
+	private NettyClient netty;
+	
+	public Client() {
+		netty = new NettyClient("127.0.0.1", 8000);
+	}
+	
 	public static void main(String[] args) {
-		
+		new Client();
 	}
 }
