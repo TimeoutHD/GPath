43677a793856f0a3a5256811aaa04991353ea05e
robin.schupp <robin.schupp@r6011-25.schule.local>
2018-12-14 14:17:48 +0100

    Save

---
+++ src/de/pi/infodisplay/shared/packets/PacketPlayOutMessage.java
@@ -0,0 +1,33 @@
+package de.pi.infodisplay.shared.packets;
+
+import io.netty.buffer.ByteBuf;
+
+import java.io.IOException;
+
+public class PacketPlayOutMessage extends Packet {
+	
+	private String msg;
+
+	public PacketPlayOutMessage() {
+		this.id = 0;
+	}
+	
+	public PacketPlayOutMessage(String msg) {
+		this.id = 0;
+		this.msg = msg;
+	}
+
+	@Override
+	public void read(ByteBuf byteBuf) throws IOException {
+		byte[] bytes = new byte[1000000000];
+		
+		byteBuf.readBytes(bytes);
+		this.msg = String.valueOf(bytes);
+	}
+
+	@Override
+	public void write(ByteBuf byteBuf) throws IOException {
+		byteBuf.writeBytes(this.msg.getBytes());
+	}
+	
+}
