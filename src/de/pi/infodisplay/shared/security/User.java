edd446cac4a93dc850c5622f16f9ad129b3e1be0
Timeout <taubmann870@gmail.com>
2019-03-26 17:31:57 +0100

    Linting changes

--- src/de/pi/infodisplay/shared/security/User.java
+++ src/de/pi/infodisplay/shared/security/User.java
@@ -4,7 +4,9 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.sql.SQLException;
 import java.util.UUID;
+import java.util.logging.Level;
 
+import de.pi.infodisplay.Main;
 import de.timeout.libs.MySQL;
 
 public class User {
@@ -26,7 +28,7 @@ public class User {
 			String[][] table = db.executeStatement("SELECT * FROM users WHERE name = ?", name);
 			return new User(UUID.fromString(table[0][0]), table[0][1], table[0][2], db, true);
 		} catch (SQLException e) {
-			e.printStackTrace();
+			Main.LOG.log(Level.SEVERE, "Cannot result SQL-Statement", e);
 		}
 		return null;
 	}
@@ -36,7 +38,7 @@ public class User {
 				if(!exists()) mysql.executeVoidStatement("INSERT INTO users(uuid, name, password) VALUES(?,?,?)", id.toString(), name, password);
 				else mysql.executeVoidStatement("UPDATE name = ?, password = ? FROM users WHERE uuid = ?", name, password, id.toString());
 			} catch (SQLException e) {
-				e.printStackTrace();
+				Main.LOG.log(Level.SEVERE, "Cannot result SQL-Statement", e);
 			}
 	}
 	
@@ -70,13 +72,11 @@ public class User {
 			md.update(pw.getBytes());
 			byte[] bytes = md.digest();
 	        StringBuilder sb = new StringBuilder();
-	        for(int i=0; i< bytes.length ;i++)
-	        {
+	        for(int i=0; i< bytes.length ;i++) 
 	        	sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
-	        }
 	        return sb.toString();
 		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
+			Main.LOG.log(Level.SEVERE, "Cannot get encode code algorythm SHA-256", e);
 			return null;
 		}
 	}
